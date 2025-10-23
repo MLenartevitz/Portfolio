739d7002b4a1d30029c829d1296e08e07aac421c
Mikael Lenartevitz <mikael.lenar123@hotmail.com>
2025-10-22 21:18:10 -0300

    modificações basicas e novas atualizações

--- src/script.js
+++ src/script.js
@@ -1,3 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const img = document.getElementById('f1');
+    let cliques = 0;
+  
+    img.addEventListener('click', () => {
+      cliques++;
+  
+      if (cliques === 12) {
+        img.src = "../img/Myicon2.png"; 
+        cliques = 0; 
+      }
+    });
+  });
 document.querySelectorAll('nav ul li a').forEach(item => {
     item.addEventListener('click', function () {
         document.querySelector('nav ul li a.ativo').classList.remove('ativo');
